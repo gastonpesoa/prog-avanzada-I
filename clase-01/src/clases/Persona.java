137e3dab4a60a599e99466853548b8cf9ad7c332
PesoaGaston <pesoagaston@MacBook-Pro-de-PesoaGaston.local>
2021-03-29 21:05:52 -0300

    clase 1 y 2

--- clase-01/src/clases/Persona.java
+++ clase-01/src/clases/Persona.java
@@ -34,7 +34,8 @@ public class Persona {
 	public boolean equals(Object o) {
 		if(o == null) {
 			return false;
-		} else if(o instanceof Persona) {
+		//} else if(o instanceof Persona) { // esto se puede mejorar
+		} else if(o.getClass().equals(this.getClass())) {
 			Persona p = (Persona)o;
 			if(this.dni == p.dni && this.nombre.equals(p.nombre)) {
 				return true;
@@ -44,5 +45,19 @@ public class Persona {
 		} else {
 			return false;
 		}
+	}
+	
+	@Override
+	public int hashCode() {
+		final int primo = 21; // final > inalterable
+		int retorno = 0;
+		retorno = primo * this.dni;
+		retorno += primo * this.nombre.hashCode();
+		return retorno;
+	}
+	
+	@Override
+	public String toString() {
+		return "Dni: " + this.dni + " - Nombre: " + this.nombre;
 	}
 }
--- clase-01/src/clases/Program.java
+++ clase-01/src/clases/Program.java
@@ -8,11 +8,12 @@ public class Program {
 		per1.setDni(123);
 		String nombrePer1 = new String("Jessica");
 		per1.setNombre(nombrePer1);
+		System.out.println(per1.toString());
 		
-		Persona per2 = new Persona("Gaston", 123);
-		
+		Persona per2 = new Persona("Gaston", 1234);
 		//syso
 		System.out.println(per1.getNombre());
+		System.out.println(per2.toString());
 				
 		if(per1 == per2) {
 			System.out.println("Son iguales");
@@ -24,9 +25,12 @@ public class Program {
 		//Para saber si un objeto es equivalente con el otro
 		if(per1.equals(per2)) {
 			System.out.println("Son iguales");
+			System.out.println(per1.hashCode());
+			System.out.println(per2.hashCode());
 		} else {
 			System.out.println("No son iguales");
-			//siempre va a caer aca porque el == compara referencias
+			System.out.println(per1.hashCode());
+			System.out.println(per2.hashCode());
 		}
 		
 		
